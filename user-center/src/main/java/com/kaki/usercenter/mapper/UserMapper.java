62cb0cac5bbd00fc8df55ff8a2f56f029874e035
kaki <cooolkaki@gmail.com>
2024-05-06 19:42:05 +0900

    Initialization

---
+++ user-center/src/main/java/com/kaki/usercenter/mapper/UserMapper.java
@@ -0,0 +1,8 @@
+package com.kaki.usercenter.mapper;
+
+import com.baomidou.mybatisplus.core.mapper.BaseMapper;
+import com.kaki.usercenter.model.User;
+
+public interface UserMapper extends BaseMapper<User> {
+
+}
